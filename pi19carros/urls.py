71a4c2ceabaf6943acccb751a7d384ef67450527
Marcelo <marcelomarinho2014.7@gmail.com>
2019-09-06 11:14:44 -0300

    Adcionei as paginas

--- pi19carros/urls.py
+++ pi19carros/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from core.views import home
 
+
 urlpatterns = [
     path('', home, name = 'home'),
        path('admin/', admin.site.urls),
