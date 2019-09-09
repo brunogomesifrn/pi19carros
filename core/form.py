90f0938841867c7f77225523597b76c44c799c06
Marcelo <marcelomarinho2014.7@gmail.com>
2019-09-09 09:29:46 -0300

    alterei a pagina cadastro

--- core/form.py
+++ core/form.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from django import forms
 
 class UserForm(ModelForm):
 	class Meta:
 		model = User
-		fields = ['first_name', 'last_name', 'email', 'password']
+		fields = ['first_name', 'last_name', 'email', 'password', 'is_active', 'is_staff', 'is_superuser']
+		widgets = {
+		'is_active': forms.HiddenInput(),
+		'is_staff': forms.HiddenInput(),
+		'is_superuser': forms.HiddenInput(),
+		}
+		
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .form import UserForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
 
 def home(request):
 	return render(request, 'index.html')
@@ -10,9 +12,10 @@ def categoria(request):
 def perfil(request):
 	return render(request, 'perfil.html')
 
-def login_cadastro(request):
+def cadastro(request):
+	logout(request)
 	dados = {}
-	form = UserForm()
+	form = UserForm(initial={'is_active':True})
 	dados['form'] = form
 
 	if request.method == 'POST':
@@ -20,7 +23,26 @@ def login_cadastro(request):
 		if form.is_valid():
 			user = form.save(commit=False)
 			user.username = user.first_name
+			user.is_staff = True
+			user.is_superuser = True
 			user.save()
 			return redirect('home')
 
 	return render(request, 'Login_Cadastro.html', dados)
+
+
+def login_(request):
+	logout(request)
+	if request.method == 'POST':
+		username = request.POST['first_name']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		print(user)
+		
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			return HttpResponse("erro")
+
+	return redirect('cadastro')
